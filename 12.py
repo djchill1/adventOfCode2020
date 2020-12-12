8ea8bf3dc529d84f449681fedbb8b74a6ee83b03
djchill1 <djchill10@googlemail.com>
2020-12-12 15:36:35 +0000

    day 12 complete

--- 12.py
+++ 12.py
@@ -38,6 +38,44 @@ def calculate_movement(instruction, facing):
     return movement, facing
 
 
+def calculate_movement_2(instruction, ship_location, waypoint_location):
+    ship_movement = [0, 0]
+    i, j = waypoint_location
+    command = instruction[0]
+    amount = int(instruction[1:])
+    cw_angle_calculator = {
+        90: [-j, i],
+        180: [-i, -j],
+        270: [j, -i]
+    }
+    ccw_angle_calculator = {
+        270: [-j, i],
+        180: [-i, -j],
+        90: [j, -i]
+    }
+    directions = {
+        'N': [amount, 0],
+        'S': [-amount, 0],
+        'E': [0, amount],
+        'W': [0, -amount]
+    }
+    if command == 'L':
+        # left = anticlockwise (negative)
+        waypoint_location = ccw_angle_calculator[amount]
+    elif command == 'R':
+        # right = clockwise (positive)
+        waypoint_location = cw_angle_calculator[amount]
+    elif command in directions:
+        # use directions dict
+        waypoint_movement = directions[command]
+        waypoint_location = np.add(waypoint_location, waypoint_movement)
+    elif command == 'F':
+        # understand compass direction then use directions dict
+        ship_movement = [amount*x for x in waypoint_location]
+        ship_location = np.add(ship_movement, ship_location)
+    # print(command, amount, facing, movement)
+    return waypoint_location, ship_location
+
 def part1(instructions):
     location = [0, 0]
     facing = 90
@@ -45,7 +83,7 @@ def part1(instructions):
         movement, facing = calculate_movement(instruction, facing)
         location = np.add(location, movement)
 
-    print(location)
+    # print(location)
 
     return abs(location[0]) + abs(location[1])
 
@@ -53,7 +91,9 @@ def part1(instructions):
 def part2(instructions):
     ship_location = [0, 0]
     waypoint_location = [1, 10]
-
-    return False
+    for instruction in instructions:
+        waypoint_location, ship_location = calculate_movement_2(instruction, ship_location, waypoint_location)
+        # print(instruction, waypoint_location, ship_location)
+    return abs(ship_location[0]) + abs(ship_location[1])
 
 print(f'Part 1: {part1(data)}, Part 2: {part2(data)}')
