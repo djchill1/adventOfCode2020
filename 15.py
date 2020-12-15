d10e6991771795eec7a36867abc04dd893900031
Jack Clare <jack.clare1@tesco.com>
2020-12-15 11:00:46 +0000

    day 15 complete

--- 15.py
+++ 15.py
@@ -1,11 +1,10 @@
 data = [0, 13, 16, 17, 1, 10, 6]  # actual input
 
 # data = [0, 3, 6]  # test data
-# data = [2,1,3]
+# data = [2,1,3] # test data
 
 def next_number(past_numbers):
 	number_being_considered = past_numbers[-1]
-	# print(number_being_considered, past_numbers)
 	indices = [i for i, x in enumerate(past_numbers) if x == number_being_considered]
 	if len(indices) == 1:
 		next_number = 0
@@ -23,9 +22,12 @@ def part1(numbers):
 
 
 def part2(numbers):
-	while len(numbers) < 30000000:
-		numbers = next_number(numbers)
-	return numbers[30000000 - 1]
+	steps = 30000000
+	num_dict = {n: i for i, n in enumerate(numbers)}
+	last = numbers[-1]
+	for n in range(len(numbers) - 1, steps - 1):
+		num_dict[last], last = n, n - num_dict.get(last, n)
+	return last
 
 
 print(f'Part 1: {part1(data)}, Part 2: {part2(data)}')
